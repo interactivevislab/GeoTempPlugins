57ff25c2e1244a08549cae2d82c628df5814302b
kir1251 <golubev1251@gmail.com>
2020-03-31 10:55:19 +0300

    GeoTempVis GeoTempRoads module added

---
+++ GeoTempVis/Source/GeoTempRoads/GeoTempRoads.Build.cs
@@ -0,0 +1,63 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+using System.IO;
+using UnrealBuildTool;
+
+public class GeoTempRoads : ModuleRules
+{
+	public GeoTempRoads(ReadOnlyTargetRules Target) : base(Target)
+	{
+
+
+
+
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core", "RuntimeMeshComponent", "GeoTempCore", "GeoTempPostgis"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+		if (Target.Type == TargetRules.TargetType.Editor)
+		{
+			PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"ProceduralMeshComponentEditor",
+				"MeshDescription",
+					"MeshDescriptionOperations",
+					"AssetTools",
+					"AssetRegistry",
+			}
+			);
+		}
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Core",
+															"CoreUObject",
+															"Engine",
+															"RenderCore",
+															"GeoTempCore", "GeoTempPostgis" });
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ GeoTempVis/Source/GeoTempRoads/Public/Road.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Road.generated.h"
+
+
+enum class EHighwayType : uint8;
+struct FApiRoadSegment;
+
+UENUM(BlueprintType)
+enum class RoadType : uint8 
+{
+	Rail	= 0 UMETA(DisplayName = "Rail"),
+	Asphalt	= 1 UMETA(DisplayName = "Asphalt"),
+	Dirt1	= 2 UMETA(DisplayName = "Dirt1"),
+	Dirt2	= 3 UMETA(DisplayName = "Dirt2"),
+	Brick	= 4 UMETA(DisplayName = "Brick"),
+	Stone	= 5 UMETA(DisplayName = "Stone"),
+	Sand	= 6 UMETA(DisplayName = "Sand")
+};
+
+
+USTRUCT(BlueprintType)
+struct GEOTEMPROADS_API FApiRoadLine
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly) FVector Start;
+	UPROPERTY(BlueprintReadOnly) FVector End;
+	UPROPERTY(BlueprintReadOnly) TArray<FVector> AllPoints;
+};
+
+
+
+
+
+USTRUCT(BlueprintType)
+struct GEOTEMPROADS_API	FApiRoadProperties
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(BlueprintReadOnly) EHighwayType Highway;
+	UPROPERTY(BlueprintReadOnly) float Angle;
+	UPROPERTY(BlueprintReadOnly) float Length;
+	UPROPERTY(BlueprintReadOnly) int ParentId;
+};
+
+
+USTRUCT(BlueprintType)
+struct GEOTEMPROADS_API	FApiRoadSegment
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly) FApiRoadLine Line;
+	UPROPERTY(BlueprintReadOnly) EHighwayType Highway;
+	UPROPERTY(BlueprintReadOnly) int Lanes;
+	UPROPERTY(BlueprintReadOnly) float LaneWidth;
+	UPROPERTY(BlueprintReadOnly) int YearStart;
+	UPROPERTY(BlueprintReadOnly) int YearEnd;
+	UPROPERTY(BlueprintReadOnly) FString Change;
+};
+
+
+USTRUCT(BlueprintType)
+struct GEOTEMPROADS_API	FApiRoadNetwork
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly) TMap<int, FApiRoadSegment> Segments;
+};
