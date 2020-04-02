743484f80e12b1038f5ef45e8ad41364516b95c1
VladLeonov <vlad_leonov@bk.ru>
2020-04-02 11:48:35 +0300

    Styling in MasksLoader.

--- GeoTempCore/Source/PolygonMasksGenerator/Private/MasksLoader.cpp
+++ GeoTempCore/Source/PolygonMasksGenerator/Private/MasksLoader.cpp
@@ -1,4 +1,5 @@
 #include "MasksLoader.h"
+
 #include "RHIStaticStates.h"
 #include "Engine/Texture2D.h"
 #include "PipelineStateCache.h"
@@ -6,101 +7,93 @@
 #include "Engine/StaticMesh.h"
 #include "RenderTargetPool.h"
 
+
 //It seems to be the convention to expose all vertex declarations as globals, and then reference them as externs in the headers where they are needed.
 //It kind of makes sense since they do not contain any parameters that change and are purely used as their names suggest, as declarations :)
 TGlobalResource<FVertexDeclarationExample> GTextureVertexDeclarationMask;
 
 
-//class FPositionVertexData :
-//	public TStaticMeshVertexData<FMyTextureVertex>
-//{
-//public:
-//	FPositionVertexData(bool InNeedsCPUAccess = false)
-//		: TStaticMeshVertexData<FMyTextureVertex>(InNeedsCPUAccess)
-//	{
-//	}
-//};
-
-
 TResourceArray<FMyTextureVertex>* dataMask;
+
 
 UMaskLoader::UMaskLoader()
 {
 	FeatureLevel = ERHIFeatureLevel::SM5;
-	FColor StartColor = FColor::Red;
 
 	ConstantParameters = FPsConstParams();
-
 	VariableParameters = FPsVarParams();
 
-	bMustRegenerateSRV = false;
-	bIsPixelShaderExecuting = false;
-	bIsUnloading = false;
+	MustRegenerateSRV		= false;
+	IsPixelShaderExecuting	= false;
+	IsUnloading				= false;
 
-	CurrentTexture = NULL;
-	CurrentRenderTarget = NULL;
-	TextureParameterSRV = NULL;
+	CurrentTexture		= nullptr;
+	CurrentRenderTarget = nullptr;
+	TextureParameterSRV = nullptr;
 }
+
 
 UMaskLoader::~UMaskLoader()
 {
-	bIsUnloading = true;
+	IsUnloading = true;
 }
 
-void UMaskLoader::ExecutePixelShader(UTextureRenderTarget2D* inclTarget)
+
+void UMaskLoader::ExecutePixelShader(UTextureRenderTarget2D* inInclTarget)
 {
 	check(IsInGameThread());
 
-	if (bIsUnloading || bIsPixelShaderExecuting) //Skip this execution round if we are already executing
+	if (IsUnloading || IsPixelShaderExecuting || !inInclTarget)
+	{
 		return;
+	}
 
-	if (!inclTarget)
-		return;
+	IsPixelShaderExecuting = true;
 
-	bIsPixelShaderExecuting = true;
-
-
-
-	//Now set our runtime parameters!
-	//VariableParameters.WorldViewProj = FMatrix::Identity;	
-
-	CurrentRenderTarget = inclTarget;
-	//CurrentRenderTarget->ClearColor = FLinearColor(0, 1, 0, 0);
+	CurrentRenderTarget = inInclTarget;
 
 	//This macro sends the function we declare inside to be run on the render thread. What we do is essentially just send this class and tell the render thread to run the internal render function as soon as it can.
 	//I am still not 100% Certain on the thread safety of this, if you are getting crashes, depending on how advanced code you have in the start of the ExecutePixelShader function, you might have to use a lock :)
-	ENQUEUE_RENDER_COMMAND(FPixelShaderRunner)(
-		[&](FRHICommandListImmediate& RHICmdList)
+	ENQUEUE_RENDER_COMMAND(FPixelShaderRunner)
+	(
+		[&](FRHICommandListImmediate& outRhiCmdList)
 		{
-			ExecutePixelShaderInternal(RHICmdList, true);
-			bIsPixelShaderExecuting = false;
+			ExecutePixelShaderInternal(outRhiCmdList, true);
+			IsPixelShaderExecuting = false;
 		}
 	);
 }
 
-void UMaskLoader::ExecutePixelShaderInternal(FRHICommandListImmediate& RHICmdList, bool isExclude)
+
+void UMaskLoader::ExecutePixelShaderInternal(FRHICommandListImmediate& outRhiCmdList, bool inIsExclude)
 {
 	check(IsInRenderingThread());
 
-	if (!CurrentRenderTarget)
-		return;
-	if (!CurrentRenderTarget->GetRenderTargetResource())
-		return;
-
-	if (bIsUnloading) //If we are about to unload, so just clean up the SRV :)
+	if (!CurrentRenderTarget || !CurrentRenderTarget->GetRenderTargetResource())
 	{
-		if (NULL != TextureParameterSRV)
+		return;
+	}
+
+	//If we are about to unload, so just clean up the SRV
+	if (IsUnloading) 
+	{
+		if (TextureParameterSRV != nullptr)
 		{
 			TextureParameterSRV.SafeRelease();
-			TextureParameterSRV = NULL;
+			TextureParameterSRV = nullptr;
 		}
 
 		return;
 	}
 
-	if (Vertices.Num() == 0) return;
+	if (Vertices.Num() == 0)
+	{
+		return;
+	}
+		
 	
-	if (!vertBuf.IsValid() || Dirty) {
+	if (!VertBuf.IsValid() || Dirty)
+	{
 		Dirty = false;
 
 		dataMask = new TResourceArray<FMyTextureVertex>();
@@ -108,32 +101,29 @@ void UMaskLoader::ExecutePixelShaderInternal(FRHICommandListImmediate& RHICmdLis
 		{
 			dataMask->Add(Vertices[i]);
 		}
+
 		FRHIResourceCreateInfo info(dataMask);
-		//info.bWithoutNativeResource = false;
-		vertBuf = RHICreateVertexBuffer(sizeof(FMyTextureVertex) * Vertices.Num(), BUF_Static | BUF_Transient, info);
-		
+		VertBuf = RHICreateVertexBuffer(sizeof(FMyTextureVertex) * Vertices.Num(), BUF_Static | BUF_Transient, info);
 
 		auto triangles = TResourceArray<uint32>();
 		for (int i = 0; i < Triangles.Num(); i++)
 		{
 			triangles.Add(Triangles[i]);
-		}				
+		}
+
 		FRHIResourceCreateInfo indInfo(&triangles);
-		indBuf = RHICreateIndexBuffer(sizeof(uint32), sizeof(uint32) * Triangles.Num(), BUF_Volatile, indInfo);
+		IndBuf = RHICreateIndexBuffer(sizeof(uint32), sizeof(uint32) * Triangles.Num(), BUF_Volatile, indInfo);
 	}
 
-	//FRHICommandListImmediate& RHICmdList = GRHICommandList.GetImmediateCommandList();
-
-
 	//If our input texture reference has changed, we need to recreate our SRV
-	if (bMustRegenerateSRV)
+	if (MustRegenerateSRV)
 	{
-		bMustRegenerateSRV = false;
+		MustRegenerateSRV = false;
 
-		if (NULL != TextureParameterSRV)
+		if (TextureParameterSRV != nullptr)
 		{
 			TextureParameterSRV.SafeRelease();
-			TextureParameterSRV = NULL;
+			TextureParameterSRV = nullptr;
 		}
 
 		TextureParameterSRV = RHICreateShaderResourceView(TextureParameter, 0);
@@ -141,77 +131,64 @@ void UMaskLoader::ExecutePixelShaderInternal(FRHICommandListImmediate& RHICmdLis
 
 	// This is where the magic happens
 	TShaderMapRef<FVertexShaderExample> VertexShader(GetGlobalShaderMap(FeatureLevel));
-	TShaderMapRef<FPixelShaderExample> PixelShader(GetGlobalShaderMap(FeatureLevel));
+	TShaderMapRef<FPixelShaderExample>	PixelShader	(GetGlobalShaderMap(FeatureLevel));
 	CurrentRenderTarget->UpdateResourceImmediate();	
 	CurrentTexture = CurrentRenderTarget->GetRenderTargetResource()->GetRenderTargetTexture();
 	
-	FPooledRenderTargetDesc Desc(FPooledRenderTargetDesc::Create2DDesc(FIntPoint(CurrentTexture->GetSizeX(), CurrentTexture->GetSizeY()), PF_DepthStencil, FClearValueBinding::DepthFar, TexCreate_None, TexCreate_DepthStencilTargetable, false));
+	FPooledRenderTargetDesc Desc(FPooledRenderTargetDesc::Create2DDesc(FIntPoint(CurrentTexture->GetSizeX(), 
+		CurrentTexture->GetSizeY()), PF_DepthStencil, FClearValueBinding::DepthFar, 
+		TexCreate_None, TexCreate_DepthStencilTargetable, false));
+
 	Desc.AutoWritable = false;
 	TRefCountPtr<IPooledRenderTarget> DepthRenderTarget;
-	GRenderTargetPool.FindFreeElement(RHICmdList, Desc, DepthRenderTarget, TEXT("DepthDummy"), true, ERenderTargetTransience::NonTransient);
+	GRenderTargetPool.FindFreeElement(outRhiCmdList, Desc, DepthRenderTarget, TEXT("DepthDummy"), 
+		true, ERenderTargetTransience::NonTransient);
 
-	
-	//FRHIRenderPassInfo RPInfo(CurrentTexture, ERenderTargetActions::DontLoad_Store, FTextureRHIRef());
-	FRHIRenderPassInfo RPInfo(CurrentTexture, ERenderTargetActions::DontLoad_Store, DepthRenderTarget->GetRenderTargetItem().TargetableTexture, EDepthStencilTargetActions::DontLoad_StoreDepthStencil);
-	RHICmdList.BeginRenderPass(RPInfo, TEXT("TestTestTest"));
+	FRHIRenderPassInfo RPInfo(CurrentTexture, ERenderTargetActions::DontLoad_Store, 
+		DepthRenderTarget->GetRenderTargetItem().TargetableTexture, 
+		EDepthStencilTargetActions::DontLoad_StoreDepthStencil);
+
+	outRhiCmdList.BeginRenderPass(RPInfo, TEXT("TestTestTest"));
+
 	{
 		FGraphicsPipelineStateInitializer GraphicsPSOInit;
-		RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
-		//GraphicsPSOInit.BlendState = TStaticBlendState<CW_RGBA, BO_Add, BF_InverseDestAlpha, BF_InverseSourceAlpha, BO_Add, BF_One, BF_One>::GetRHI();
-		GraphicsPSOInit.BlendState = TStaticBlendState<CW_RGBA, BO_Max, BF_One, BF_One, BO_Max, BF_One, BF_One>::GetRHI();
-		GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
-		GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<true, CF_LessEqual>::GetRHI();
-		GraphicsPSOInit.PrimitiveType = PT_TriangleList;
-		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GTextureVertexDeclarationMask.VertexDeclarationRhi;
-		GraphicsPSOInit.BoundShaderState.VertexShaderRHI = GETSAFERHISHADER_VERTEX(*VertexShader);
-		GraphicsPSOInit.BoundShaderState.PixelShaderRHI = GETSAFERHISHADER_PIXEL(*PixelShader);
+		outRhiCmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
 
+		GraphicsPSOInit.BlendState			= TStaticBlendState<CW_RGBA, BO_Max, BF_One, BF_One, BO_Max, BF_One, BF_One>::GetRHI();
+		GraphicsPSOInit.RasterizerState		= TStaticRasterizerState<>::GetRHI();
+		GraphicsPSOInit.DepthStencilState	= TStaticDepthStencilState<true, CF_LessEqual>::GetRHI();
+		GraphicsPSOInit.PrimitiveType		= PT_TriangleList;
+		GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI	= GTextureVertexDeclarationMask.VertexDeclarationRhi;
+		GraphicsPSOInit.BoundShaderState.VertexShaderRHI		= GETSAFERHISHADER_VERTEX(*VertexShader);
+		GraphicsPSOInit.BoundShaderState.PixelShaderRHI			= GETSAFERHISHADER_PIXEL(*PixelShader);
 
-		PixelShader->SetOutputTexture(RHICmdList, TextureParameterSRV);		
-		VertexShader->SetUniformBuffers(RHICmdList, ConstantParameters, VariableParameters);
-		PixelShader->SetUniformBuffers(RHICmdList, ConstantParameters, VariableParameters);		
-		SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);		
+		PixelShader->SetOutputTexture(outRhiCmdList, TextureParameterSRV);
+		VertexShader->SetUniformBuffers(outRhiCmdList, ConstantParameters, VariableParameters);
+		PixelShader	->SetUniformBuffers(outRhiCmdList, ConstantParameters, VariableParameters);
+		SetGraphicsPipelineState(outRhiCmdList, GraphicsPSOInit);
 		
-		RHICmdList.SetViewport(
-			0, 0, -500,
-			CurrentTexture->GetSizeX(), CurrentTexture->GetSizeY(), 500.f);
-		
-		RHICmdList.SetStreamSource(0, vertBuf, 0);		
-		
-		RHICmdList.DrawIndexedPrimitive(indBuf, 0, 0, Vertices.Num(), 0, Triangles.Num() / 3, 0);
+		outRhiCmdList.SetViewport(0, 0, -500, CurrentTexture->GetSizeX(), CurrentTexture->GetSizeY(), 500.f);
+		outRhiCmdList.SetStreamSource(0, VertBuf, 0);
+		outRhiCmdList.DrawIndexedPrimitive(IndBuf, 0, 0, Vertices.Num(), 0, Triangles.Num() / 3, 0);
 	}
-	RHICmdList.EndRenderPass();
 
+	outRhiCmdList.EndRenderPass();
 
-	//DrawPrimitiveUP(RHICmdList, PT_TriangleStrip, 2, Vertices, sizeof(Vertices[0]));
+	PixelShader->UnbindBuffers(outRhiCmdList);
 
-	PixelShader->UnbindBuffers(RHICmdList);
-
-	// Resolve render target.
-	//RHICmdList.CopyToResolveTarget(
-	//	CurrentRenderTarget->GetRenderTargetResource()->GetRenderTargetTexture(),
-	//	CurrentRenderTarget->GetRenderTargetResource()->TextureRHI,
-	//	FResolveParams());
-
-
-	//if (bSave) //Save to disk if we have a save request!
-	//{
-	//	bSave = false;
-	//
-	//	SaveScreenshot(RHICmdList);
-	//}
-
-	bIsPixelShaderExecuting = false;
+	IsPixelShaderExecuting = false;
 }
 
 
-void UMaskLoader::UpdateRect() {
-	VariableParameters.Rect = Rect;
-}
-
-void UMaskLoader::RenderMaskForTime(int year, UTextureRenderTarget2D* inclTarget, float p)
+void UMaskLoader::UpdateRect()
 {
-	VariableParameters.Year = year + p;
 	VariableParameters.Rect = Rect;
-	ExecutePixelShader(inclTarget);
+}
+
+
+void UMaskLoader::RenderMaskForTime(int inYear, UTextureRenderTarget2D* inInclTarget, float inP)
+{
+	VariableParameters.Year = inYear + inP;
+	VariableParameters.Rect = Rect;
+	ExecutePixelShader(inInclTarget);
 }
